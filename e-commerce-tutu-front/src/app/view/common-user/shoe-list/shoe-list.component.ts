426132e9c0d7e87b0a62eb9886ca024ab67ae273
tuliostarling <tuliostarling@hotmail.com>
2018-09-29 11:56:36 -0300

    List-one-product screen template created.

--- e-commerce-tutu-front/src/app/view/common-user/shoe-list/shoe-list.component.ts
+++ e-commerce-tutu-front/src/app/view/common-user/shoe-list/shoe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shoe-list',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoeListComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+  listOneProduct(id: number) {
+    this.router.navigateByUrl('product/' + id);
   }
 
 }
