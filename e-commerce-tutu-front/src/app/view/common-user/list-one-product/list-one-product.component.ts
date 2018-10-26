c7b158835880ddb7bf7cd9125eb1e619ba9ba0bf
tuliostarling <tuliostarling@hotmail.com>
2018-10-26 18:17:18 -0300

    ListOne screen integrated with backend. Still needs some minor changes

--- e-commerce-tutu-front/src/app/view/common-user/list-one-product/list-one-product.component.ts
+++ e-commerce-tutu-front/src/app/view/common-user/list-one-product/list-one-product.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { ProductService } from '../../../service';
 import { ProductModel, SubProductModel } from '../../../model/product/product';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-list-one-product',
   templateUrl: './list-one-product.component.html',
+  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./list-one-product.component.css']
 })
 export class ListOneProductComponent implements OnInit {
@@ -17,13 +19,18 @@ export class ListOneProductComponent implements OnInit {
   productName: string;
   productPrice: number;
   productColor: string;
+  productSize: any;
   productDescription: string;
   ArrImg = [];
+  imgIndex: number;
+  changeImgBool = false;
+  previousUrl: any;
 
   constructor(
     private router: Router,
     private apiService: ProductService,
-    private acRoute: ActivatedRoute
+    private acRoute: ActivatedRoute,
+    private modalService: NgbModal,
   ) { }
 
   ngOnInit() {
@@ -38,16 +45,33 @@ export class ListOneProductComponent implements OnInit {
         this.rowsProduct = res;
         this.rowsSubProduct = res;
 
-        // for (const i of Object.keys(this.rowsProduct)) {
-        //   this.ArrImg.push({ img: this.rowsSubProduct.locationaws });
-        // }
+        for (const i of Object.keys(this.rowsSubProduct.location_aws)) {
+          this.ArrImg.push({ index: i, img: this.rowsSubProduct.location_aws[i] });
+        }
 
         this.productName = this.rowsProduct.name;
         this.productPrice = this.rowsSubProduct.price;
         this.productColor = this.rowsSubProduct.color;
         this.productDescription = this.rowsProduct.description;
+        this.productSize = this.rowsSubProduct.size;
       }
     });
   }
 
+  changeImg(index: number) {
+    this.imgIndex = index;
+    this.changeImgBool = true;
+  }
+
+  cart() {
+    this.router.navigateByUrl('/cart');
+  }
+
+  productList() {
+    this.router.navigateByUrl('/category_list/4/0');
+  }
+
+  openModal(content) {
+    this.modalService.open(content, { centered: true });
+  }
 }
