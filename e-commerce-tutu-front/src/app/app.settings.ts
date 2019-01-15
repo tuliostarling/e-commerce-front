8537e40a1af2dfbedfab9b25c23b6fc6cf6fd26c
Caciquez <victorcaciquinho@gmail.com>
2019-01-15 18:54:48 -0200

    - fix

--- e-commerce-tutu-front/src/app/app.settings.ts
+++ e-commerce-tutu-front/src/app/app.settings.ts
@@ -1,10 +1,5 @@
 export class AppSettings {
-<<<<<<< HEAD
     static API_ENDPOINT = 'http://tutuguerra.com.br:3000/api/';
     //static API_ENDPOINT = 'http://localhost:3000/api/';
-=======
-    //static API_ENDPOINT = 'http://tutuguerra.com.br:3000/api/';
-    static API_ENDPOINT = 'http://localhost:3000/api/';
->>>>>>> a1e143914f182f77cd6c20a6909c45342b2b4865
     static VERSION = '1';
 }
